111fc95f710fc3485ec86d21df88b5e6500decfa
chriszat <zattechnology@gmail.com>
2020-05-07 09:30:15 +0100

    changed verification email tempalte

--- application/shared/email_template/verification.template.php
+++ application/shared/email_template/verification.template.php
@@ -1,248 +1,111 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <style type="text/css">
-    /* SPECIAL CLIENT STYLES */
+<head><style type="text/css" title="x-apple-mail-formatting"></style>
+    <title>Typeform</title>
+    <meta name="viewport" content="width = 375, initial-scale = -1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <link rel="stylesheet" href="https://font.typeform.com/dist/font-email.css">
+    <style type="text/css">
+		@font-face{font-family:apercu-pro;font-style:normal;font-weight:400;src:local("apercu-pro"),url(https://font.typeform.com/dist/fonts/ApercuPro-Regular.woff2) format("woff")}
+		body,td{font-family:Arial,Helvetica,sans-serif}
+		strong {
+    font-weight: bold;}
+		p{margin:0;padding:0 0 1em 0}
+		a{text-decoration:none}
+      	@media all and (max-width: 520px) {
+              .hide_on_mobile{display:none !important}
+			  .show_on_mobile{display:block !important;margin:auto !important; float:left !important}
+              .fullwidth{width:100% !important;height:auto !important;min-width:100% !important;float:none !important}
+              .padded{box-sizing:border-box;padding-left:10px !important;padding-right:10px !important}
+              #logo{float:none !important}
+              .button{font-size:16px !important}
+			.content td{padding-right:10px !important}
+          }
+          @media screen{
+              body,td{font-family:apercu-pro,sans-serif !important}
+          }
+    </style>
 
-    #outlook a {
-      padding: 0;
-    }
+<style type="text/css">
 
-    /* Force Outlook to provide a "view in browser" message */
+</style>
 
-    .ReadMsgBody {
-      width: 100%;
-    }
+<style type="text/css">
 
-    .ExternalClass {
-      width: 100%;
-    }
-
-    /* Force HM to display emails at full width */
-
-    .ExternalClass,
-    .ExternalClass p,
-    .ExternalClass span,
-    .ExternalClass font,
-    .ExternalClass td,
-    .ExternalClass div {
-      line-height: 100%;
-    }
-
-    /* Force HM to display normal line spacing */
-
-    body,
-    table,
-    td,
-    p,
-    a,
-    li,
-    blockquote {
-      -webkit-text-size-adjust: 100%;
-      -ms-text-size-adjust: 100%;
-    }
-
-    /* Prevent WebKit and Windows mobile changing default text sizes */
-
-    table,
-    td {
-      mso-table-lspace: 0pt;
-      mso-table-rspace: 0pt;
-    }
-
-    /* Remove spacing between tables in Outlook 2007 and up */
-
-    img {
-      -ms-interpolation-mode: bicubic;
-    }
-
-    /* Allow smoother rendering of resized image in Internet Explorer */
-
-    /* RESET STYLES */
-
-    body {
-      margin: 0;
-      padding: 0;
-    }
-
-    img {
-      border: 0 none;
-      height: auto;
-      line-height: 100%;
-      outline: none;
-      text-decoration: none;
-    }
-
-    a img {
-      border: 0 none;
-    }
-
-    .imageFix {
-      display: block;
-    }
-
-    table,
-    td {
-      border-collapse: collapse;
-    }
-
-    #bodyTable {
-      height: 100% !important;
-      margin: 0;
-      padding: 0;
-      width: 100% !important;
-    }
-
-    #footer a {
-      color: #00a4bd;
-      text-decoration: none;
-    }
-
-    /* Responsive Styles */
-
-    @media only screen and (max-width: 480px) {
-      .responsiveRow {
-        width: 100% !important;
-      }
-      .responsiveColumn {
-        display: block !important;
-        width: 100% !important;
-      }
-    }
-  </style>
-  <!--[if mso]><style type="text/css">body, table, td {
-  font-family: Helvetica Neue, Avenir Next, Arial, Helvetica, sans-serif !important;
-}</style><![endif]-->
+</style>
 </head>
+<body marginheight="0" marginwidth="0" topmargin="0" leftmargin="0" rightmargin="0" bottommargin="0" bgcolor="#FFFFFF" style="margin:0;padding:0;font-family:Arial,Helvetica,sans-serif;-webkit-text-size-adjust:none">
+	<div style="display:none;font-size:1px;color:#FFFFFF;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden">
+						<p>You’re almost there. Open me up to verify your email—and get back to making great typeforms.</p>
+		    </div>
+	  <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#FFFFFF" class="fullwidth">
+      <tbody><tr>
+        <td>
 
-<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" bgcolor="#f5f8fa" style="-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; margin: 0; padding: 0; font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 16px; height: 100%; width: 100%; min-width: 100%;">
-  <table id="outerWrapper" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" bgcolor="#f5f8fa" style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 16px; color: #425b76; line-height: 1.5; width: 100%; min-width: 100%; background-color:#f5f8fa;">
-    <tbody>
-      <tr>
-        <td align="center" valign="top">
-          <table border="0" cellpadding="20" cellspacing="0" width="600" style="width: 600px;" class="emailContainer">
-            <tbody>
-              <tr>
-                <td align="center" valign="top" width="100%" style="width: 100%; min-width: 100%;">
-                  
-                  <table id="backgroundTable" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" bgcolor="#ffffff" style="width: 100%; min-width: 100%;">
-                    <tbody>
-                      <tr>
-                        <td align="left" valign="top" style="font-size: 16px; padding: 0 50px">
-                          <table cellpadding="0" border="0" cellspacing="0" width="100%" style="color: #425b76; background-color: ; font-size: 20px; width: 100%; margin: initial; min-width: 100%; ">
-                            <tbody>
-                              <tr>
-                                <td align="center" valign="middle" style="padding: 0; ">
-                                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 50px; width: 100%; min-width: 100%; line-height: 0;">
-                                    <tbody>
-                                      <tr>
-                                        <td height="50"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                                      </tr>
-                                    </tbody>
-                                  </table><img src="https://static.hsappstatic.net/EmailNotificationSharedAssets/ex/email-forwarding.png" style="max-width: 150px">
-                                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 20px; width: 100%; min-width: 100%; line-height: 0;">
-                                    <tbody>
-                                      <tr>
-                                        <td height="20"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                  <h1 style="font-size: 24px; font-weight: 600; margin: 0; text-align: center">Please confirm your email address <br>{{email}}</h1>
-                                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 30px; width: 100%; min-width: 100%; line-height: 0;">
-                                    <tbody>
-                                      <tr>
-                                        <td height="30"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                  <hr style="height: 1px; color: #eaf0f6; background-color: #eaf0f6; border: none; margin: 0px; padding: 0px;">
-                                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 30px; width: 100%; min-width: 100%; line-height: 0;">
-                                    <tbody>
-                                      <tr>
-                                        <td height="30"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table><span style="font-weight: 600"> <i18n-string data-key="account.verifyUserEmailAddress.ifNewSignupText" data-locale-at-render="en-us">Thanks for signing up to {{sitename}}, . We’re happy to have you.</i18n-string> </span>
-                          <p style="margin: 0">
-                            <i18n-string data-key="account.verifyUserEmailAddress.bodyText" data-locale-at-render="en-us">Please take a second to make sure we have your correct email address.</i18n-string>
-                          </p>
-                      
-                          <br>
-                          <p style="margin: 0">
-                            <i18n-string data-key="account.verifyUserEmailAddress.bodyText" data-locale-at-render="en-us"><b>If the button below is not clickable, copy and paste this link in your browser </b><a href="{{link}}">{{link}}</a></i18n-string>
-                          </p>
-                          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 50px; width: 100%; min-width: 100%; line-height: 0;">
-                            <tbody>
-                              <tr>
-                                <td height="50"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                              </tr>
-                            </tbody>
-                          </table>
-                          <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                            <tbody>
-                              <tr>
-                                <td align="center">
-                                  <table border="0" cellspacing="0" cellpadding="0">
-                                    <tbody>
-                                      <tr>
-                                        <td align="center" style="border-radius: 3px;" bgcolor="#425b76" width="auto"><a href="{{link}}"
-                                            target="_blank" style="border: 1px solid #425b76; border-radius: 3px; color: #FFFFFF; display: inline-block; font-size: 14px; font-weight: 500; line-height: 1; padding: 12px 20px; text-decoration: none; width: auto; min-width: 170px; white-space: nowrap; ">Confirm your email address</a></td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                          <div itemscope itemtype="http://schema.org/EmailMessage">
-                            <div itemprop="potentialAction" itemscope itemtype="http://schema.org/ViewAction">
-                              <link itemprop="target" href="https://app.hubspot.com/verify?u=matthew@fathomanddraft.com&amp;t=GS9W_PGY96UmfKORtn3bAYwx65_Ut5sDWEblVD6vmC2u3RJOe063BDGxcvBPBGTC&amp;i=8092426">
-                              <meta itemprop="name" content="Confirm your email address">
-                            </div>
-                            <meta itemprop="description" content="Confirm your email address">
-                          </div>
-                          <div itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
-                            <meta itemprop="name" content="HubSpot">
-                            <link itemprop="url" content="https://www.hubspot.com">
-                            <link itemprop="url/googlePlus" content="https://plus.google.com/+hubspot/">
-                          </div>
-                          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 20px; width: 100%; min-width: 100%; line-height: 0;">
-                            <tbody>
-                              <tr>
-                                <td height="20"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                              </tr>
-                            </tbody>
-                          </table>
-                         
-                          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0; height: 50px; width: 100%; min-width: 100%; line-height: 0;">
-                            <tbody>
-                              <tr>
-                                <td height="50"><span style="-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; color: transparent; background: none; user-select: none; -moz-user-select: none; -ms-user-select:none; -webkit-user-select:none; text-overflow: ellipsis; opacity: 0; width:100%; min-width: 100%; height:1; overlfow:hidden; margin: -1px 0 0 0; padding:0; font-size: 0;"> &nbsp;</span></td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  
-  
-  
+      		<table border="0" cellpadding="0" cellspacing="0" width="600" class="fullwidth" align="center" style="margin:auto">
+        	<tbody><tr>
+          		<td style="padding:30px 30px 20px 0px" class="masthead padded">
+
+
+              </td>
+            </tr>
+            <tr>
+              <td class="content">
+
+				<div mc:repeatable="Hero Section">
+
+					<table border="0" cellpadding="0" cellspacing="0" width="100%">
+					<tbody><tr>
+						<td style="padding:0 30px" class="padded">
+
+							<table border="0" cellpadding="0" cellspacing="0" width="100%" class="fullwidth" style="border-bottom:1px solid #e7e7e7">
+							<tbody><tr>
+								<td style="padding:20px 0;text-align:left;font-weight:500;font-size:24px;color:#343333" class="avenir" mc:edit="title">Hello, you’re a click away</td>
+							  </tr>
+							  <tr>
+								<td style="padding:0 80px 0 0;font-size:18px;line-height:167%;font-weight:400;color:#343333" mc:edit="copy 1">
+																						<p>Hit the button to verify your email—and get back to bitcoin investment 💪</p>
+																		 </td>
+							  </tr>
+							  <tr>
+								<td style="padding:30px 0 60px 0" mc:edit="button">
+
+									<!--[if mso]><table border="0" cellpadding="0" cellspacing="0" align="left" bgcolor="#343333"><tr><td align="left"><![endif]-->
+									<div align="left">
+										<a data-qa="verification_link" href="{{link}}" style="text-decoration:none;color:#FFFFFF;background-color:#343333;display:inline-block;padding:15px 20px;margin:auto;border:2px solid #343333;border-radius:3px;font-size:18px;font-weight:500" class="button">
+																							Verify my email
+																					</a>
+									</div>
+									<!--[if mso]></td></tr></table><![endif]-->
+
+								</td>
+							</tr>
+							</tbody></table>
+
+							<table border="0" cellpadding="0" cellspacing="0" width="100%" class="fullwidth">
+							<tbody><tr>
+								<td style="padding:20px 80px 0 0;font-size:16px;line-height:167%;font-weight:400;color:#343333" mc:edit="copy 2">
+									<p>This link will self-destruct in 2 hours.<br> Didn’t ask for this email? Just ignore me.								</p></td>
+							</tr>
+							</tbody></table>
+
+						</td>
+					</tr>
+					</tbody></table>
+
+                </div>
+
+			</td>
+        	</tr>
+          	</tbody></table>
+
+		</td>
+ 	</tr>
+    </tbody></table>
+
+
+
+
+
+
 </body>
-
-</html>
