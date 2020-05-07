d53e4fb3d6017e0829c34999abb2dbc9dcee1186
chriszat <zattechnology@gmail.com>
2020-05-07 09:44:45 +0100

    udpated

--- application/shared/email_template/verification.template.php
+++ application/shared/email_template/verification.template.php
@@ -1,3 +1,7 @@
-<h3>Click on the link to confirm your email account</h3>
 
-<p><a>{{link}}</a></p>
+<h2>Let Verify Your Email</h2>
+
+<p><a href="{{link}}">{{link}}</a></p>
+
+
+<h3>If you didnt make this request, just ignore this message</h3>
