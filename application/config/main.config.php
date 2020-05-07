a783aebd1f596ce27653442d2ee29b3eeb6fdc81
chriszat <zattechnology@gmail.com>
2020-05-07 20:57:34 +0100

    changed sendgrid api key

--- application/config/main.config.php
+++ application/config/main.config.php
@@ -4,4 +4,4 @@ require_once "database.php";
 /**
  * set sendgrid api key in environmental variable
  */
-putenv('SENDGRID_API_KEY=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');
+putenv('SENDGRID_API_KEY=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');
