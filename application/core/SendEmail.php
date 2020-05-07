8212cb47a42f5e4be9341bba2acb0ec784a8ff41
chriszat <zattechnology@gmail.com>
2020-05-07 09:06:41 +0100

    fixed email bugs

--- application/core/SendEmail.php
+++ application/core/SendEmail.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Send Email class
  */
@@ -24,25 +25,26 @@ class SendEmail
 
     public function configuration()
     {
-        $config=[];
+        $config = [];
         $settings = $this->getter->settings();
         // $config["name_from_email"]=explode("@", $info['email'])[0];
-        $config["username"]=$settings['from_email'];
-        $config["password"]=$settings['email_password'];;
-        $config["sitename"]=$settings['sitename'];
-        $config["smtp"] =$settings['email_smtp'];
+        $config["username"] = $settings['from_email'];
+        $config["password"] = $settings['email_password'];;
+        $config["sitename"] = $settings['sitename'];
+        $config["smtp"] = $settings['email_smtp'];
         return $config;
     }
 
     public function send_mail($email, $subject, $message, $alt)
     {
-        
+
         // $name = stripslashes($_POST['name']);
         $email = $email;
-        $message = stripslashes($message);
+        $message = $message;
         $url = baseurl;
 
         $e_subject = $subject;
+        $settings = $this->getter->settings();
 
 
         // Configuration option.
@@ -56,16 +58,27 @@ class SendEmail
 
         $msg = wordwrap($e_body . $e_reply, 70);
 
-        $headers = "From: $email" . PHP_EOL;
-        $headers .= "Reply-To: $email" . PHP_EOL;
-        $headers .= "Website: $url" . PHP_EOL;
-        $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
-        $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+        // $headers = "MIME-Version: 1.0" . "\r\n"; 
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
 
-        if (mail($email, $msg, $headers, $e_content)) {
-             echo json_encode(array('status'=>'success', "message"=>"Email sent."));
-        }else{
-             echo json_encode(array('status'=>'error', "message"=>"Could not send mail to that address"));
+        // $headers .= "From: $email" . PHP_EOL;
+        // $headers .= "Reply-To: $email" . PHP_EOL;
+        // $headers .= "Website: $url" . PHP_EOL;
+        // To send HTML mail, the Content-type header must be set
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+        // Additional headers
+        //$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+        $headers[] = 'From: ' . $settings['mailing_email'];
+        //$headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
+        //$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+        if (mail($email, $subject,  $message, implode("\r\n", $headers))) {
+
+            echo json_encode(array('status' => 'success', "message" => "Email sent."));
+        } else {
+            echo json_encode(array('status' => 'error', "message" => "Could not send mail to that address"));
         }
     }
 
@@ -82,7 +95,7 @@ class SendEmail
             //Server settings
             //$mail->SMTPDebug = 2;                               // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host = $smtp;				  // Specify main and backup SMTP servers
+            $mail->Host = $smtp;                  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = $username;           // SMTP username
             $mail->Password = $password;                // SMTP password
@@ -90,16 +103,16 @@ class SendEmail
             $mail->Port = 587;                                    // TCP port to connect to
 
             $mail->SMTPOptions = array(
-            'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-            )
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
             );
 
             //Recipients
-            $email=$email;
-            $mail->setFrom('noreply@'.$sitename.'.localhost', $sitename);
+            $email = $email;
+            $mail->setFrom('noreply@' . $sitename . '.localhost', $sitename);
             $mail->addAddress($email);     // Add a recipient
             //$mail->addReplyTo('informaljoke@gmail.com', 'Asifor');
             //$mail->addCC('cc@example.com');
@@ -112,47 +125,46 @@ class SendEmail
             $mail->Body    = $message;
             $mail->AltBody = $alt;
             $mail->send();
-            echo json_encode(array('status'=>'success', "message"=>"Verification email sent."));
-        }catch (Exception $e) {
+            echo json_encode(array('status' => 'success', "message" => "Verification email sent."));
+        } catch (Exception $e) {
             print_r($e);
-            echo json_encode(array('status'=>'error', "message"=>"Could not send mail to that address"));
+            echo json_encode(array('status' => 'error', "message" => "Could not send mail to that address"));
             $mail->ErrorInfo;
         }
     }
 
-    public function send_verification_email($email, $subject, $data=[], $alt)
+    public function send_verification_email($email, $subject, $data = [], $alt)
     {
-        
+
         $message = $this->load->load_email_template("../application/shared/email_template/verification.template.php", $data);
         $message = str_replace(["{{sitename}}", "{{email}}", "action_url", "{{link}}"], [$data["sitename"], $email, $alt, $data["link"]], $message);
 
         $verify = $this->send_mail($email, $subject, $message, $alt);
     }
 
-    public function secondary_email_verification($email, $subject, $data=[], $alt)
+    public function secondary_email_verification($email, $subject, $data = [], $alt)
     {
-        $message = $this->load->load_email_template
-        ("../application/shared/email_template/secondary_email_verification.template.php", $data);
+        $message = $this->load->load_email_template("../application/shared/email_template/secondary_email_verification.template.php", $data);
         $message = str_replace(["sitename", "toemail", "action_url", "link", "#user"], [$data["sitename"], $email, $alt, $data["link"], $data["username"]], $message);
         print_r($data);
 
         $verify = $this->send_mail($email, $subject, $message, $alt);
     }
 
-    public function send_btc_transfer_notification($email, $subject, $data=[], $alt)
+    public function send_btc_transfer_notification($email, $subject, $data = [], $alt)
     {
-        
+
         $message = $this->load->load_email_template("../application/shared/email_template/transfer_notification.php", $data);
         $message = str_replace(["{{sitename}}", "{{username}}", "{{btc_address}}", "{{btc_value}}", "{{transaction}}", "{{currency}}", "{{logo}}"], [$data["sitename"], $data['username'], $data['btc_address'], $data["btc_value"], $data['transaction'], $data["currency"], $data["logo"]], $message);
 
         $verify = $this->send_mail($email, $subject, $message, $alt);
     }
 
-    public function send_btc_mining_success($email, $subject, $data=[], $alt)
+    public function send_btc_mining_success($email, $subject, $data = [], $alt)
     {
-        
+
         $message = $this->load->load_email_template("../application/shared/email_template/btc_mine_success.php", $data);
-        $message = str_replace(["{{sitename}}", "{{username}}", "{{btc_address}}", "{{btc_reward}}",  "{{logo}}", "{{dashboard_link}}","{{year}}", "{{mining_plan}}", "{{time_mined}}", "{{support_link}}", "{{ref_code}}", ], [$data["sitename"], $data['username'], $data['btc_address'], $data["btc_value"],  $data["logo"], $data["dashboard_link"], $data["year"], $data["mining_plan"], $data["time_mined"], $data["support_link"], $data["ref_code"]], $message);
+        $message = str_replace(["{{sitename}}", "{{username}}", "{{btc_address}}", "{{btc_reward}}",  "{{logo}}", "{{dashboard_link}}", "{{year}}", "{{mining_plan}}", "{{time_mined}}", "{{support_link}}", "{{ref_code}}",], [$data["sitename"], $data['username'], $data['btc_address'], $data["btc_value"],  $data["logo"], $data["dashboard_link"], $data["year"], $data["mining_plan"], $data["time_mined"], $data["support_link"], $data["ref_code"]], $message);
 
         $verify = $this->send_mail($email, $subject, $message, $alt);
     }
@@ -165,7 +177,6 @@ class SendEmail
 
     public function generate_email_token()
     {
-        return sha1(str_shuffle("abcdefghijklmnopqrstuvwxyz").time());
+        return sha1(str_shuffle("abcdefghijklmnopqrstuvwxyz") . time());
     }
-
 }
